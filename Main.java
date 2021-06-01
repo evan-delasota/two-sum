793aa4108e6c3ba44a5f92246c61b288cf4f1911
Evan D <Evan_delasota@yahoo.com>
2021-06-01 02:38:00 +0000

    Solution to twoSum using a hashmap, O(n) runtime.

--- Main.java
+++ Main.java
@@ -1,5 +1,31 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.lang.Exception;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9 , 10};
+    int[] sol = twoSum(arr, 18);
+
+    System.out.println("Solution: " + sol[0] + " " + sol[1]);
   }
+
+  public static int[] twoSum(int[] arr, int target) {
+    Map<Integer, Integer> seen = new HashMap<Integer, Integer>();
+
+    for (int i = 0; i < arr.length; ++i) {
+      seen.put(arr[i], i);
+    }
+
+    for (int i = 0; i < arr.length; ++i) {
+      int complement = target - arr[i];
+      if (seen.containsKey(complement) && seen.get(complement) != i) {
+        return new int[] { i, seen.get(complement) };
+      }
+    }
+
+    throw new IllegalArgumentException("No solution found");
+  }
+
 }
